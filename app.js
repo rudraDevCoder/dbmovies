45120a4e127cb086cd8051ada51e79a45af0bbde
rudra <rudra8997@gmail.com>
2023-03-23 10:38:11 +0000

    updatedjsfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 
 const app = express();
-
+const up = express.json();
 const dbPath = path.join(__dirname, "moviesData.db");
 let db = null;
 const initializeDBAndServer = async () => {
@@ -13,8 +13,8 @@ const initializeDBAndServer = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(4000, () => {
-      console.log("Server Running at http://localhost:4000/");
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
     });
   } catch (e) {
     console.log(`DB Error: ${e.message}`);
@@ -25,9 +25,21 @@ initializeDBAndServer();
 app.get("/movies/", async (request, response) => {
   const getMovieNameQuery = `
     SELECT
-      movie_name
+      *
     FROM
       movie`;
   const movieArray = await db.all(getMovieNameQuery);
   response.send(movieArray);
+});
+app.post("/movies/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const updateMovie = `INSERT INTO movie
+    (director_id,movie_name,lead_actor)
+    VALUES
+    ('${directorId}',
+    '${movieName}',
+    '${leadActor}');`;
+  const updateQuery = await db.run(updateMovie);
+  const movie_id = updateQuery.lastId;
+  response.send(updateQuery);
 });
